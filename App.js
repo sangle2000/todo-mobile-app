1cb7726c985e16cc64516d3ecd67b706dd6cf7ca
sangle <thanhsang22440011@gmail.com>
2025-05-22 21:33:38 +0700

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setGoal(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    if (goal.trim().length === 0) {
+      return;
+    }
+    setGoals((currentGoals) => [...currentGoals, goal]);
+    setGoal('');
+  }
+
+  const deleteGoalHandler = (goalIndex) => {
+    setGoals((currentGoals) => {
+      return currentGoals.filter((_, index) => index !== goalIndex);
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput value={goal} onChangeText={goalInputHandler} style={styles.textInput} placeholder='Your course goal!' />
+        <Button onPress={addGoalHandler} title='Add Goal' />
+      </View>
+      <View style={styles.goalsContainer}>
+        {
+          goals.length > 0 ? goals.map((g, index) => <Text onPress={() => deleteGoalHandler(index)} key={index}>{g}</Text>) : <Text>No goals added yet!</Text>
+        }
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 16,
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 3,
+  }
 });
